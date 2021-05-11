80e33c7d59cee58e199e4b35217582b263824378
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-05-11 23:45:28 +0530

    Add raw layout

---
+++ src/Components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Welcome extends React.Component {
+    render() {
+        return(
+            <div>Welcome to TheCrypt!</div>
+        )
+    }
+}
+
+export default Welcome
