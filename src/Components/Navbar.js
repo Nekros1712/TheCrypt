0a062802775793293b6cebe02fceb5f6c56d44fa
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-05-14 23:52:44 +0530

    Add Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Navbar() {
+
+    const navStyle = {
+        "position": "absolute",
+        "margin": "3vh 0vw 0vh 2vw",
+        "display": "flex",
+        "justifyContent": "center"
+    }
+
+    return (
+        <div style={ navStyle }>
+            <ham-burger style={{ "height": "3vh", "width": "4vw" }}>
+                <svg viewBox="0 0 120 120">
+                    <line x1="10" y1="0" x2="100" y2="0" style={{ "stroke": "#03dac5", "stroke-width": "11" }} />
+                    <line x1="10" y1="25" x2="100" y2="25" style={{ "stroke": "#03dac5", "stroke-width": "5.5" }} />
+                    <line x1="10" y1="50" x2="100" y2="50" style={{ "stroke": "#03dac5", "stroke-width": "6.5" }} />
+                </svg>
+            </ham-burger>
+            <the-crypt style={ { "color": "#fff", "fontSize": "40px", "marginTop": "-2vh", "marginLeft": "1vw" } }>
+                <span>The</span>
+                <span style={ { "color": "#03DAC5" } }>Crypt</span>
+            </the-crypt>
+        </div>
+    )
+}
+
+export default Navbar
