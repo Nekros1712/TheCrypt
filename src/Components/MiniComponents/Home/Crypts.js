8f28916722bbe5a2ca40ad140555c2b2b8a7ad67
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-05-26 20:43:02 +0530

    Fetch API

--- src/Components/MiniComponents/Home/Crypts.js
+++ src/Components/MiniComponents/Home/Crypts.js
@@ -6,11 +6,43 @@ function Crypts() {
 
     const currency = { "margin": "12vh 0vw 0vh 5vw", "width": "60vw", "height": "45vh" }
 
+    const baseUrl = "https://api.coinranking.com/v2/coins"
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    const apiKey = 'coinranking5c3322b13eeb05b688b8e7d972034c20e3149cd7cd74c4f4'
+
+    // let coins
+
+    fetch(`${proxyUrl}${baseUrl}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': `${apiKey}`,
+            'Access-Control-Allow-Origin': '*'
+        }
+    })
+    .then((response) => {
+        if(response.ok) {
+            response.json().then((res) => {
+                // let coins = res.data.coins
+                console.table(res.data.coins)
+                res.data.coins.map(coin => (
+                    `<tr key=${coin.uuid}>
+                        <td>${coin.name}</td>
+                        <td>${coin.price}</td>
+                        <td>${coin.change}</td>
+                    </tr>`
+                ))
+            })
+        }
+    })
+    .catch((err) => {console.log(err)})
+
+
     return (
-        <card className="card" style={ currency }>
+        <div className="card" style={ currency }>
             <card-head>
                 <card-title>Crypts</card-title>
-                <currency-converter style={ { "text-align": "right" } }>
+                <currency-converter style={ { "textAlign": "right" } }>
                     Currency: <current-currency>INR</current-currency>
                 </currency-converter>
             </card-head>
@@ -26,8 +58,8 @@ function Crypts() {
 
             <table className="data-table">
                 <tbody>
-                    {data.map(data => (
-                        <tr>
+                {data.map(data => (
+                        <tr key={data.id}>
                             <td>{data.name}</td>
                             <td>{data.price}</td>
                             <td>{data.growth}</td>
@@ -35,7 +67,7 @@ function Crypts() {
                     ))}
                 </tbody>
             </table>
-        </card>
+        </div>
     )
 }
 
