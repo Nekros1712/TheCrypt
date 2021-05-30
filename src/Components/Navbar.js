adf96f93810f6e76812ef77987eafb01c549bcbd
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-05-30 23:02:40 +0530

    Route using react-router-dom

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import '../style/Navbar.css'
 
 function Navbar() {
+
+    const navStyle = { "textDecoration": "none", "color": "#ffffff" }
 
     return (
         <div className="navStyle">
@@ -16,10 +19,26 @@ function Navbar() {
             <div className="nav">
                 <span>The<span style={ { "color": "#03DAC5" } }>Crypt</span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
                 <div className="navs">
-                    <span className="active">Home</span>
-                    <span>Compare</span>
-                    <span>About</span>
-                    <span>Contact</span>
+                    <span className="nav-item">
+                        <NavLink style={ navStyle } exact to="/">
+                            <span id="home">Home</span>
+                        </NavLink>
+                    </span>
+                    <span className="nav-item">
+                        <NavLink style={ navStyle } to="/compare">
+                            <span id="compare">Compare</span>
+                        </NavLink>
+                    </span>
+                    <span className="nav-item">
+                        <NavLink style={ navStyle } to="/about">
+                            <span id="about">About</span>
+                        </NavLink>
+                    </span>
+                    <span className="nav-item">
+                        <NavLink style={ navStyle } to="/contact">
+                            <span id="contact">contact</span>
+                        </NavLink>
+                    </span>
                 </div>
                 <div className="bag-btn">
                     <button>Populate Bag</button>
