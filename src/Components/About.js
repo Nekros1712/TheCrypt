1c9a2724131fca4d51aa4e006ca5a5e77fe34758
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-06-01 23:30:19 +0530

    Add Images - About

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,15 +1,28 @@
 import React from 'react'
+
+import '../style/About.css'
 import '../style/Card.css'
+import Logo from '../data/Logo.png'
+import Hero from '../data/Hero.png'
 
 function About() {
 
-    const TheCrypt = { "margin": "20vh 0vw 0vh 10vw", "width": "50vw", "height": "37vh" }
-    const Developer = { "margin": "65vh 0vw 0vh 45vw", "width": "40vw", "height": "30vh" }
+    const about = { "margin": "20vh 0vw 0vh 10vw", "width": "50vw", "height": "37vh" }
+    const logo = { "margin": "20vh 0vw 0vh 65vw", "height": "37vh", "width": "20vw" }
+    const hero = { "margin": "65vh 0vw 0vh 18vw", "width": "200px", "height": "200px" }
+    const dev = { "margin": "65vh 0vw 0vh 37vw", "width": "40vw", "height": "30vh" }
 
     return (
         <div>
-            <div className="card" style={ TheCrypt }></div>
-			<div className="card" style={ Developer }></div>
+            <div className="card about" style={ about }>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur maxime temporibus molestiae accusantium nam iusto amet reiciendis laboriosam consequuntur? Quas numquam fugiat harum odio eaque nam natus minus nesciunt rem.</p>
+            </div>
+            <img src={ Logo } style={ logo } alt="Logo"></img>
+            <div className="role">Developer</div>
+            <img src={ Hero } style={ { ...hero, "borderRadius": "50%" } } alt="Logo"></img>
+			<div className="card about" style={ dev }>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente aliquam ducimus illum aperiam, qui quibusdam.</p>
+            </div>
         </div>
     )
 }
