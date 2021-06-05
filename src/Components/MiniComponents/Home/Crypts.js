8deb774dec2d9b4f92ac852de6352faff1e4f715
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-06-05 23:34:56 +0530

    Connect New API - Home

--- src/Components/MiniComponents/Home/Crypts.js
+++ src/Components/MiniComponents/Home/Crypts.js
@@ -4,9 +4,8 @@ function Crypts() {
 
     const currency = { "margin": "12vh 0vw 0vh 5vw", "width": "60vw", "height": "45vh" }
 
-    const baseUrl = "https://api.coinranking.com/v2/coins"
+    const baseUrl = "https://api.coingecko.com/api/v3/coins"
     const proxyUrl = "https://cors-anywhere.herokuapp.com/"
-    const apiKey = 'coinranking5c3322b13eeb05b688b8e7d972034c20e3149cd7cd74c4f4'
 
     let coinData = ''
 
@@ -14,7 +13,6 @@ function Crypts() {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'x-access-token': `${apiKey}`,
             'Access-Control-Allow-Origin': '*'
         }
     })
@@ -22,10 +20,10 @@ function Crypts() {
         if(response.ok) {
             response.json().then((res) => {
 
-                res.data.coins.forEach((coin) => {
+                res.forEach((coin) => {
 
-                    let price = Math.round((parseFloat(coin.price) + Number.EPSILON) * 1000) / 1000
-                    let change = Math.round((parseFloat(coin.change) + Number.EPSILON) * 100) / 100
+                    let price = Math.round((parseFloat(coin.market_data.current_price.usd) + Number.EPSILON) * 1000) / 1000
+                    let change = Math.round((parseFloat(coin.market_data.price_change_percentage_24h) + Number.EPSILON) * 100) / 100
 
                     let bgcolor = '#1F1B24'
                     let color = 'white'
@@ -35,9 +33,9 @@ function Crypts() {
                         color = 'black'
                     }
 
-                    coinData += `<tr key=${coin.uuid}>`
+                    coinData += `<tr key=${coin.id}>`
                     coinData +=     `<td>${coin.name}</td>`
-                    coinData +=     `<td>${price}</td>`
+                    coinData +=     `<td class="price-column">${price}</td>`
                     coinData +=     `<td style=color:${color};background-color:${bgcolor};border-radius:5px;>${change}</td>`
                     coinData += `</tr>`
 
@@ -48,6 +46,28 @@ function Crypts() {
         }
     })
     .catch((err) => {console.log(err)})
+
+
+    // finalResult
+    // fetch("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1&interval=hourly")
+    // .then(res => {
+    // let result = []
+    // res.json().then(ans => {
+    //     ans.prices.map((data, index) => {
+    //     let date = formatter(data[0])
+    //     let price = Math.round((parseFloat(data[1]) + Number.EPSILON)*1000)/1000
+    //     result.push([date, price])
+    //     })
+    //     finalResult = [...result]
+    // })
+    // })
+
+    // function formatter(timestamp) {
+    // var date = new Date(timestamp).getDate()
+    // var month = new Date(timestamp).getMonth() + 1
+    // var hour = new Date(timestamp).getHours()
+    // return date + "/" + month + ": " + hour;
+    // }
 
 
     return (
