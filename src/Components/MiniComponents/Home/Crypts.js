189dcdd511c50b6d54dfe0783bb60a95c9ba1800
Samprit1712 <sampritchaurasiya07@gmail.com>
2021-06-07 21:43:39 +0530

    Replace fetch by axios - Home

--- src/Components/MiniComponents/Home/Crypts.js
+++ src/Components/MiniComponents/Home/Crypts.js
@@ -1,74 +1,44 @@
 import React from 'react'
+import axios from 'axios'
 
 function Crypts() {
 
     const currency = { "margin": "12vh 0vw 0vh 5vw", "width": "60vw", "height": "45vh" }
 
     const baseUrl = "https://api.coingecko.com/api/v3/coins"
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    let coinData = ""
 
-    let coinData = ''
+    axios
+        .get(baseUrl)
+        .then(res => {
+            res.data.map((coin) => {
 
-    fetch(`${proxyUrl}${baseUrl}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
-    })
-    .then((response) => {
-        if(response.ok) {
-            response.json().then((res) => {
+                let price = Math.round((parseFloat(coin.market_data.current_price.usd) + Number.EPSILON) * 1000) / 1000
+                let change = Math.round((parseFloat(coin.market_data.price_change_percentage_24h) + Number.EPSILON) * 100) / 100
 
-                res.forEach((coin) => {
+                let bgcolor = '#1F1B24'
+                let color = 'white'
 
-                    let price = Math.round((parseFloat(coin.market_data.current_price.usd) + Number.EPSILON) * 1000) / 1000
-                    let change = Math.round((parseFloat(coin.market_data.price_change_percentage_24h) + Number.EPSILON) * 100) / 100
+                if(change < 0) {
+                    bgcolor = '#CF6679'
+                    color = 'black'
+                }
 
-                    let bgcolor = '#1F1B24'
-                    let color = 'white'
+                coinData += `<tr key=${coin.id}>`
+                coinData +=     `<td>${coin.name}</td>`
+                coinData +=     `<td class="price-column">${price}</td>`
+                coinData +=     `<td style=color:${color};background-color:${bgcolor};border-radius:5px;>${change}</td>`
+                coinData += `</tr>`
 
-                    if(change < 0) {
-                        bgcolor = '#CF6679'
-                        color = 'black'
-                    }
+                return null
 
-                    coinData += `<tr key=${coin.id}>`
-                    coinData +=     `<td>${coin.name}</td>`
-                    coinData +=     `<td class="price-column">${price}</td>`
-                    coinData +=     `<td style=color:${color};background-color:${bgcolor};border-radius:5px;>${change}</td>`
-                    coinData += `</tr>`
-
-                })
-
-                document.getElementById('coins').innerHTML = coinData
             })
-        }
-    })
-    .catch((err) => {console.log(err)})
 
-
-    // finalResult
-    // fetch("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1&interval=hourly")
-    // .then(res => {
-    // let result = []
-    // res.json().then(ans => {
-    //     ans.prices.map((data, index) => {
-    //     let date = formatter(data[0])
-    //     let price = Math.round((parseFloat(data[1]) + Number.EPSILON)*1000)/1000
-    //     result.push([date, price])
-    //     })
-    //     finalResult = [...result]
-    // })
-    // })
-
-    // function formatter(timestamp) {
-    // var date = new Date(timestamp).getDate()
-    // var month = new Date(timestamp).getMonth() + 1
-    // var hour = new Date(timestamp).getHours()
-    // return date + "/" + month + ": " + hour;
-    // }
-
+            document.getElementById("coins").innerHTML = coinData
+        })
+        .catch(err => {
+            console.log(err);
+        })
 
     return (
         <div className="card" style={ currency }>
